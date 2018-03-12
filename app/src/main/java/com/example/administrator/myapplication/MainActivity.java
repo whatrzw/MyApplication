e3aa96e81a06f2e8fe20a21d948e958923cea76f
whatrzw <rzw_286636693@163.com>
2018-03-12 22:47:42 +0800

    测试 额问问他

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("aa", "维恩");
+        Log.e("aa", "额问问他");
     }
 }
